f67c0e62456124fae218dd1f5e856979bc4300c6
EricBorder <magicsergy@gmail.com>
2022-11-14 10:42:02 +0100

    Terminado

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var empezarJugar: Button
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
