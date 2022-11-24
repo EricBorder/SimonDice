034e693c36b9a8e001415c3a709d0fbc4b1844e6
EricBorder <magicsergy@gmail.com>
2022-11-24 13:12:59 +0100

    Añado clase DataBase

---
+++ app/src/main/java/com/example/simondice/RecordDataBase.kt
@@ -0,0 +1,12 @@
+package com.example.simondice
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Record::class],
+    version = 1
+)
+abstract class RecordDataBase : RoomDatabase() {
+    abstract fun recordDao() : RecordDAO
+}
