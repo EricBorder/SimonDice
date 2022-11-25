a9cd529bf662503b8a5ed7d3bf9804329aba414d
EricBorder <magicsergy@gmail.com>
2022-11-25 13:15:44 +0100

    Añado clase para el RecordDAO

--- app/src/main/java/com/example/simondice/Record.kt
+++ app/src/main/java/com/example/simondice/Record.kt
@@ -11,14 +11,4 @@ data class Record(
     val puntuacion: Int
 )
 
-interface RecordDAO {
 
-    @Query("SELECT puntuacion FROM Record WHERE id = 1")
-    fun getPuntuacion(): Int
-
-    @Query("INSERT INTO Record (puntuacion) VALUES (0)")
-    fun crearPuntuacion()
-
-    @Update
-    fun update(record: Record)
-}
